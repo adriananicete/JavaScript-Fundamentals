613bd41fc5b7826c6126af7fdcc69f03a1431412
Adrian Anicete <anicete.ian@gmail.com>
2026-02-05 18:33:05 +0800

    dom manipulation

---
+++ dom/dom-manipulation/index.js
@@ -0,0 +1,57 @@
+// Creating Elements
+const hTwo = document.createElement("h2");
+hTwo.innerText = "Hello World";
+document.body.appendChild(hTwo);
+console.log(hTwo);
+
+const hThree = document.createElement("h3");
+hThree.innerText = "Hi world";
+document.body.appendChild(hThree);
+
+// Insert Elements
+const span = document.createElement("span");
+span.innerText = "Adrian";
+document.body.insertBefore(span, hThree);
+
+console.log(span.nextElementSibling);
+
+// Removing Elements
+const list = document.getElementById("myList");
+console.log(list.children.length);
+
+const random = Math.floor(Math.random() * list.children.length);
+console.log(random);
+
+const itemToRemove = list.children[random];
+console.log(itemToRemove);
+list.removeChild(itemToRemove);
+
+span.remove(); // direct removal
+
+// Read Write and Remove Attribute
+{
+  const img = document.querySelector("img");
+  console.log(img.getAttribute("src"));
+  console.log(img.getAttribute("alt"));
+  img.setAttribute("alt", "asdfsdf");
+  img.setAttribute('src', 'https://res.cloudinary.com/dks2psaem/image/upload/v1760445840/git9o_oxv35r.png')
+  img.removeAttribute('height')
+}
+
+
+function changeSize(){
+    const img = document.querySelector("img");
+    img.setAttribute('width','500px')
+}
+
+function defaultSize(){
+    const img = document.querySelector("img");
+    img.setAttribute('width','400px')
+}
+
+function changeImg(){
+    const img = document.querySelector("img");
+    img.setAttribute('src','https://res.cloudinary.com/dks2psaem/image/upload/v1770189612/Screenshot_2026-02-04_151855_jjjcr8.png')
+}
+
+// Traverisng/Navigation DOM
