049bb33a74b5eb1387428a6ba51bfdcbd0c56c29
Adrian Anicete <anicete.ian@gmail.com>
2026-02-05 21:24:06 +0800

    completed

--- dom/dom-manipulation/index.js
+++ dom/dom-manipulation/index.js
@@ -1,34 +1,34 @@
 // Creating Elements
-const hTwo = document.createElement("h2");
-hTwo.innerText = "Hello World";
-document.body.appendChild(hTwo);
-console.log(hTwo);
+// const hTwo = document.createElement("h2");
+// hTwo.innerText = "Hello World";
+// document.body.appendChild(hTwo);
+// console.log(hTwo);
 
-console.log('next sibling',hTwo.nextSibling)
+// console.log('next sibling',hTwo.nextSibling)
 
-const hThree = document.createElement("h3");
-hThree.innerText = "Hi world";
-document.body.appendChild(hThree);
+// const hThree = document.createElement("h3");
+// hThree.innerText = "Hi world";
+// document.body.appendChild(hThree);
 
 // Insert Elements
-const span = document.createElement("span");
-span.innerText = "Adrian";
-document.body.insertBefore(span, hThree);
+// const span = document.createElement("span");
+// span.innerText = "Adrian";
+// document.body.insertBefore(span, hThree);
 
-console.log(span.nextElementSibling);
+// console.log(span.nextElementSibling);
 
-// Removing Elements
-const list = document.getElementById("myList");
-console.log(list.children.length);
+// // Removing Elements
+// const list = document.getElementById("myList");
+// console.log(list.children.length);
 
-const random = Math.floor(Math.random() * list.children.length);
-console.log(random);
+// const random = Math.floor(Math.random() * list.children.length);
+// console.log(random);
 
-const itemToRemove = list.children[random];
-console.log(itemToRemove);
-list.removeChild(itemToRemove);
+// const itemToRemove = list.children[random];
+// console.log(itemToRemove);
+// list.removeChild(itemToRemove);
 
-span.remove(); // direct removal
+// span.remove(); // direct removal
 
 
 // Read Write and Remove Attribute
@@ -75,4 +75,84 @@ function changeImg(){
 
 
 
+}
+
+// Manipulating Classes
+{
+    const mainId = document.getElementById('main-id');
+    console.log(mainId.className)
+
+    // mainId.className = 'secondary-class'
+    // console.log(mainId.className)
+
+    console.log(mainId.classList)
+    mainId.classList.add('test')
+    console.log(mainId.className)
+
+    mainId.classList.remove('layout')
+    console.log(mainId.className)
+
+    // replace use for replacing single class on an element tag
+    mainId.classList.replace('main-class','secondary-class')
+    console.log(mainId.className)
+
+    // contains use to check if a element tag has a class on it it returns boolean
+    console.log('does it have test class?', mainId.classList.contains('test'))
+
+    console.log('does it have main-class?', mainId.classList.contains('main-class'))
+
+    // toggle use for on and off of a class in an element tag
+    mainId.classList.toggle('secondary-class')
+    mainId.classList.toggle('secondary-class')
+    mainId.classList.toggle('secondary-class')
+    mainId.classList.toggle('secondary-class')
+
+
+}
+
+function toggleInfo(){
+    console.log('toggling...')
+
+    const para = document.getElementById('myParagraph')
+    para.classList.toggle('hidden')
+}
+
+function addTask(){
+    const inputTask = document.getElementById('taskInput');
+    const task = inputTask.value
+    console.log(task)
+
+    const li = document.createElement('li')
+    li.textContent = task
+
+    const completeBtn = document.createElement('button')
+    completeBtn.textContent = 'Complete'
+    completeBtn.onclick = () => li.classList.add('completed')
+    li.appendChild(completeBtn)
+
+    const editBtn = document.createElement('button')
+    editBtn.textContent = 'Edit'
+    editBtn.onclick = () => {
+        const inputEdit = document.createElement('input')
+        inputEdit.value = task
+        li.appendChild(inputEdit)
+    }
+    li.appendChild(editBtn)
+
+    const btn = document.createElement('button')
+    btn.textContent = 'Delete'
+    btn.onclick = () => li.remove()
+    li.appendChild(btn)
+
+    
+    
+    document.getElementById('taskList').appendChild(li)
+    inputTask.value = ''
+}
+
+function filterTask() {
+    const searchInput = document.getElementById('searchInput').value;
+    const taskList = document.getElementById('taskList')
+
+    taskList.style.display = taskList.innerText.toLowerCase().includes('searchInput') ? 'block' : 'none'
 }
